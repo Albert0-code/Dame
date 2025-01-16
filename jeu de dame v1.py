17f13837ae020067a99329ab35385796b1539827
Owen <owenbuffard@gmail.com>
2025-01-16 11:25:06 +0100

    Update jeu de dame v1.py paranthese en plus

--- jeu de dame v1.py
+++ jeu de dame v1.py
@@ -11,10 +11,10 @@ def initialisation ():
            [0,2,0,2,0,2,0,2,0,2]]
 
 def jouer (table):
-    ligne_depart = int(input("Quelle est la ligne du pion que vous voulez déplacer ?")
-    colonne_depart = int(input("Quelle est sa colonne ?")
-    ligne_arriver = int(input("Vers quelle ligne ?")
-    colonne_arriver = int(input("Vers quelle colonne ?")
+    ligne_depart = int(input("Quelle est la ligne du pion que vous voulez déplacer ?"))
+    colonne_depart = int(input("Quelle est sa colonne ?"))
+    ligne_arriver = int(input("Vers quelle ligne ?"))
+    colonne_arriver = int(input("Vers quelle colonne ?"))
     table[ligne_arriver][colonne_arriver] = table[ligne_depart][colonne_depart]
     table[ligne_depart][colonne_depart] = 0
     print(table)
